27a051bc092c94c2137d27df98f0df4ea1e3d9fd
bweben <nathanael@ibw-informatik.ch>
2020-03-09 08:38:19 +0100

    feat: implement Encoder and Decoder

--- src/li/naw/rsa/decode/Decoder.java
+++ src/li/naw/rsa/decode/Decoder.java
@@ -1,9 +1,32 @@
 package li.naw.rsa.decode;
 
 import li.naw.rsa.model.RSAPrivateKey;
+import li.naw.rsa.utils.FastExponentiation;
+
+import java.math.BigInteger;
+import java.util.stream.Collectors;
 
 public class Decoder {
-    public String decode(String toDecode, RSAPrivateKey privateKey) {
-        return "";
+    private final RSAPrivateKey privateKey;
+
+    public Decoder(RSAPrivateKey privateKey) {
+        this.privateKey = privateKey;
+    }
+
+    public String decode(String toDecode) {
+        return toDecode.chars()
+                .mapToObj(this::encode)
+                .collect(Collectors.joining());
+    }
+
+    private String encode(int asciiCode) {
+        return String.valueOf(
+                Character.highSurrogate(
+                        new FastExponentiation(BigInteger.valueOf(asciiCode),
+                                privateKey.getD().intValueExact(),
+                                privateKey.getN())
+                                .calculateMod()
+                )
+        );
     }
 }
--- src/li/naw/rsa/encode/Encoder.java
+++ src/li/naw/rsa/encode/Encoder.java
@@ -1,9 +1,33 @@
 package li.naw.rsa.encode;
 
 import li.naw.rsa.model.RSAPublicKey;
+import li.naw.rsa.utils.FastExponentiation;
+
+import java.math.BigInteger;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Encoder {
-    public String encode(String toEncode, RSAPublicKey publicKey) {
-        return "";
+    private final RSAPublicKey publicKey;
+
+    public Encoder(RSAPublicKey publicKey) {
+        this.publicKey = publicKey;
+    }
+
+    public String encode(String toEncode) {
+        return toEncode.chars()
+                .mapToObj(this::encode)
+                .collect(Collectors.joining());
+    }
+
+    private String encode(int asciiCode) {
+        return String.valueOf(
+                Character.highSurrogate(
+                        new FastExponentiation(BigInteger.valueOf(asciiCode),
+                                publicKey.getE().intValueExact(),
+                                publicKey.getN())
+                                .calculateMod()
+                )
+        );
     }
 }
